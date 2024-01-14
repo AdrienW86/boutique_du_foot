4dd3a60403688e4230dfe95e5d5f388e049e4a0a
AdrienW86 <adrien_weiss@outlook.fr>
2024-01-14 13:59:21 +0100

    mise en ligne

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import '@/styles/globals.css'
+import Head from 'next/head'
 import Header from '@/components/Header/Header'
 import Nav from '@/components/Nav/Nav'
 import Footer from '@/components/Footer/Footer'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <> 
+    <Head>
+        <title>La boutique du foot</title>
+        <meta name="description" content="La boutique en ligne du football" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />       
+        <link rel="icon" href="/favicon.ico" />     
+      </Head>
     <Header />
     <Nav />
     <Component  {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Accueil from '@/components/Accueil/Accueil'
 import styles from '@/styles/Home.module.css'
@@ -8,12 +7,6 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>La boutique du foot</title>
-        <meta name="description" content="La boutique en ligne du football" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />       
-        <link rel="icon" href="/favicon.ico" />     
-      </Head>
       <main className={`${styles.main} ${inter.className}`}>
        <Accueil />
       </main>
