34f5bc462714133b4f75b166d3747b674e4ee9a9
AdrienW86 <adrien_weiss@outlook.fr>
2024-02-18 04:22:44 +0100

    mise en video

--- components/Media/Media.jsx
+++ components/Media/Media.jsx
@@ -5,7 +5,7 @@ import styles from './media.module.css';
 function BannerVideo(props) {
   return (
     <div className={styles.banner}>
-      <video controls className={styles.banner} preload="auto">
+      <video controls className={styles.banner} preload="auto" playsInline>
         <source src={props.content} type="video/mp4" />
         Votre navigateur ne supporte pas la lecture de vidéos au format MP4.
       </video>
