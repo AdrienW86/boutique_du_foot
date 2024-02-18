4a61ebec1e44b2ad02b32c01db6c2052a833a67d
AdrienW86 <adrien_weiss@outlook.fr>
2024-02-18 01:56:12 +0100

    mise en ligne

--- components/Media/Media.jsx
+++ components/Media/Media.jsx
@@ -7,7 +7,7 @@ import styles from './media.module.css'
 function BannerVideo() {
   return (
     <div className={styles.banner}>
-      <video controls autoPlay className={styles.banner}>
+      <video controls className={styles.banner}>
         <source src={Nasser} type="video/mp4" />
         Votre navigateur ne supporte pas la lecture de vidéos au format MP4.
       </video>
